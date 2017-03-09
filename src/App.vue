91986bccf1fd265ccd9808bc9b6aaaef9e252ac1
Austin <austin@thisisvisceral.com>
2017-03-09 12:06:10 -0800

    Named slots

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-xs-12">
                 <appQuote>
-                    <h2>{{ quoteTitle }}</h2>
-                    <p>A Wonderful Quote!</p>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p slot="content">A Wonderful Quote!</p>
                 </appQuote>
             </div>
         </div>
