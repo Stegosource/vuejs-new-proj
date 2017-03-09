1b059af4e886dfecbc826256260f074573e27ec5
Austin <austin@thisisvisceral.com>
2017-03-09 11:50:28 -0800

    Section 9 - Advanced Component Usage

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue({
-  data: {
-
-  },
-  methods: {
-    changeAge: function(age) {
-      this.$emit('ageWasChanged', age);
-    }
-  }
-});
-
 new Vue({
   el: '#app',
   render: h => h(App)
-});
+})
