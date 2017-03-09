213db2c5ead487e1cf5af1f4a50f8b935ce6d0d8
Austin <austin@thisisvisceral.com>
2017-03-09 12:08:36 -0800

    Default slots and default content

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
             <div class="col-xs-12">
                 <appQuote>
                     <h2 slot="title">{{ quoteTitle }}</h2>
-                    <p slot="content">A Wonderful Quote!</p>
+                    <p>A Wonderful Quote!</p>
                 </appQuote>
             </div>
         </div>
