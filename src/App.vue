e3a60e979e7f7504f15ea9387d0fb8ec647bd7a2
Austin <austin@thisisvisceral.com>
2017-03-09 12:19:47 -0800

    Using conditional components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <appQuote>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <p>{{ selectedComponent }}</p>
+                <hr>
+                <component :is="selectedComponent">
+                    <p>Default Content</p>
+                </component>
+                <!-- <appQuote>
                     <h2 slot="title">{{ quoteTitle }}</h2>
                     <p>A Wonderful Quote!</p>
-                </appQuote>
+                </appQuote> -->
             </div>
         </div>
     </div>
@@ -13,15 +21,20 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function() {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
             };
         },
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New,
         }
     }
 </script>
