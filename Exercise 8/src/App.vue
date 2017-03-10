f02d2bffb31ddc21772ce283013086a78faf96bc
Austin <austin@thisisvisceral.com>
2017-03-09 16:02:46 -0800

    Exercise 8 Finished

--- Exercise 8/src/App.vue
+++ Exercise 8/src/App.vue
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="component = 'app-blue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="component = 'app-green'">Load Green Template</button>
+                <button class="btn btn-danger" @click="component = 'app-red'">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="component">
+                    Content
+                </component>
             </div>
         </div>
     </div>
@@ -21,6 +21,11 @@
     import Red from './components/Red.vue';
 
     export default {
+        data: function() {
+            return {
+                component: 'app-blue'
+            };
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
