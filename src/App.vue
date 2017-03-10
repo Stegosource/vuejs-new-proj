2dccad443c6d013b4dd87bf87a5d8bd55b662456
Austin <austin@thisisvisceral.com>
2017-03-09 16:06:57 -0800

    Section 10 - Wonderful Quotes start

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,12 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <button @click="selectedComponent = 'appQuote'">Quote</button>
-                <button @click="selectedComponent = 'appAuthor'">Author</button>
-                <button @click="selectedComponent = 'appNew'">New</button>
-                <p>{{ selectedComponent }}</p>
-                <hr>
-                <keep-alive>
-                    <component :is="selectedComponent">
-                        <p>Default Content</p>
-                    </component>
-                </keep-alive>
-                <!-- <appQuote>
-                    <h2 slot="title">{{ quoteTitle }}</h2>
-                    <p>A Wonderful Quote!</p>
-                </appQuote> -->
-            </div>
-        </div>
+    
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue';
-    import Author from './components/Author.vue';
-    import New from './components/New.vue';
-
     export default {
-        data: function() {
-            return {
-                quoteTitle: 'The Quote',
-                selectedComponent: 'appQuote'
-            };
-        },
-        components: {
-            appQuote: Quote,
-            appAuthor: Author,
-            appNew: New,
-        }
+        
     }
 </script>
 
