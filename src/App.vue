c098a64a561e82f6315d7cc5697e8efe3fd1e874
Austin <austin@thisisvisceral.com>
2017-03-10 09:48:46 -0800

    Working with selects

--- src/App.vue
+++ src/App.vue
@@ -88,8 +88,11 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option 
+                            v-for="priority in priorities" 
+                            :selected="priority == 'Medium'">{{ priority }}</option>
                     </select>
                 </div>
             </div>
@@ -119,7 +122,7 @@
                             <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
                         <p>Gender: {{ gender }}</p>
-                        <p>Priority:</p>
+                        <p>Priority: {{ selectedPriority }}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -139,7 +142,9 @@
                 },
                 message: 'Message text',
                 sendMail: [],
-                gender: 'Male'
+                gender: 'Male',
+                priorities: ['High', 'Medium', 'Low'],
+                selectedPriority: 'High'
             };
         }
     }
