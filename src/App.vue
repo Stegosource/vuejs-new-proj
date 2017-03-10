eb3219d082de81434be42f5f9fc07ef7ad58c3ab
Austin <austin@thisisvisceral.com>
2017-03-09 16:28:31 -0800

    Creating Quote and QuoteGrid components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-    
+        <appQuoteGrid :quotes="quotes"></appQuoteGrid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
-        
+        data: function() {
+            return {
+                quotes: [
+                    'First quote'
+                ],
+                maxQuotes: 10,
+            };
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
     }
 </script>
 
