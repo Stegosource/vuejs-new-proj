f4164375f6b75bf6f58ecdbd2787f520e6c6076e
Austin <austin@thisisvisceral.com>
2017-03-14 13:54:46 -0700

    Exercise 9 Completed

--- Exercise 9/src/App.vue
+++ Exercise 9/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!isSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
@@ -17,21 +17,55 @@
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
+                    <appFullName v-model="userInfo.fullName"></appFullName>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input
+                                type="email"
+                                id="email"
+                                class="form-control"
+                                v-model="userInfo.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userInfo.password">
+                    </div>
+                    <div class="form-group">
+                        <label for="checkbox">Store Data?</label>
+                        <input
+                                type="checkbox"
+                                id="checkbox"
+                                class="form-control"
+                                v-model="userInfo.storeData">
+                    </div>
+
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary"
+                            @click.prevent="submitForm">Submit!
+                    </button>
                 </div>
             </div>
         </form>
-        <hr>
-        <div class="row">
+        <div class="row" v-else>
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userInfo.fullName }}</p>
+                        <p>Mail: {{ userInfo.email }}</p>
+                        <p>Password: {{ userInfo.password }}</p>
+                        <p>Store in Database?: {{ userInfo.storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +74,27 @@
 </template>
 
 <script>
+    import FullName from './FullName.vue'
     export default {
+        data() {
+            return {
+                userInfo: {
+                    fullName: 'test name',
+                    email: '',
+                    password: '',
+                    storeData: false
+                },
+                isSubmitted: false
+            }
+        },
+        components: {
+            appFullName: FullName
+        },
+        methods: {
+            submitForm() {
+                this.isSubmitted = true
+            }
+        }
     }
 </script>
 
---
+++ Exercise 9/src/FullName.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="fname">First Name</label>
+            <input
+                    type="text"
+                    id="fname"
+                    class="form-control"
+                    :value="fname"
+                    @input="nameChanged(true, $event)">
+        </div>
+        <div class="form-group">
+            <label for="lname">Last Name</label>
+            <input
+                    type="text"
+                    id="lname"
+                    class="form-control"
+                    :value="lname"
+                    @input="nameChanged(false, $event)">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    methods: {
+        nameChanged(isFirst, event) {
+            let name = '';
+            if ( isFirst ) {
+                name = event.target.value + ' ' + this.lname;
+            } else {
+                name = this.fname + ' ' + event.target.value;
+            }
+            this.value = name;
+            this.$emit('input', this.value);
+        }
+    },
+    computed: {
+        fname() {
+            return this.value.split(' ')[0];
+        },
+        lname() {
+            return this.value.split(' ')[1];
+        }
+    }
+}
+</script>
