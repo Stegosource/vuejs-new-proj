c34ff2012b475cea3c8dd16b7a43ea5cd2be3372
Austin <austin@thisisvisceral.com>
2017-03-15 14:45:23 -0700

    Registering local directives

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed="'green'">Color this too</p>
             </div>
         </div>
         <hr>
@@ -37,7 +38,27 @@
 </template>
 
 <script>
-    
+    export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    // el.style.backgroundColor = 'green';    
+                    // el.style.backgroundColor = binding.value;
+                    var delay = 0;
+                    if ( binding.modifiers['delayed'] ) {
+                    delay = 3000;
+                    }
+                    setTimeout(() => {
+                        if ( binding.arg == 'background' ) {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
+            }
+        }
+    }
 </script>
 
 <style>
