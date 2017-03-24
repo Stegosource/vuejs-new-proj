a926f823398edee8c289221565a7d815d84f9f52
Austin <austin@thisisvisceral.com>
2017-03-24 15:56:55 -0700

    Updating project from computer crash :(

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,3 +1,11 @@
 <template>
-    <h3>Edit the User</h3>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{ $route.query.locale }}</p>
+        <p>q: {{ $route.query.q }}</p>
+        <hr>
+        <button class="btn btn-primary">Confirm</button>
+        <div style="height: 700px;"></div>
+        <p id="data">Extra Data</p>
+    </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -7,8 +7,25 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if ( savedPosition ) {
+      return savedPosition;
+    }
+    if ( to.hash ) { // If the to location has a hash property
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 } // Manually scroll to location
+  }
 });
+
+router.beforeEach((to, from, next) => {
+  console.log('Global beforeEach');
+  // next(false); prevents continuing on with the path.
+  // next('/'); You can pass a hard coded URL to go to redirect to.
+  // next({}); You can pass an object where you define the path to navigate.
+  next();
+})
 
 new Vue({
   el: '#app',
