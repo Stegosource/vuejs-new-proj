fc2c68a4cf15c75788030ef6fda994ddac8ee851
Austin <austin@thisisvisceral.com>
2017-03-28 17:18:33 -0700

    Section 17 - State management with Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,30 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Routing</h1>
-				<hr>
-				<router-view name="header-top"></router-view>
-				<transition name="slide" mode="out-in">
-					<router-view></router-view></router-view>
-				</transition>
-				<router-view name="header-bottom"></router-view>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Vuex</h1>
+                <app-result :counter="counter"></app-result>
+                <hr>
+                <app-counter @updated="counter += $event"></app-counter>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	import Header from './components/Header.vue'
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
 
-	export default {
-		components: {
-			appHeader: Header
-		}
-	}
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        components: {
+            appCounter: Counter,
+            appResult: Result,
+        }
+    }
 </script>
 
-<style>
-.slide-leave-active {
-	transition: opacity 1s ease;
-	opacity: 0;
-	animation: slide-out 1s ease-out forwards;
-}
-.slide-leave {
-	opacity: 1;
-	transform: translateX(0);
-}
-.slide-enter-active {
-	animation: slide-in 1s ease-out forwards;
-}
-@keyframes slide-out {
-	from {
-		transform: translateY(0);
-	}
-	to {
-		transform: translateY(-30px);
-	}
-}
-@keyframes slide-in {
-	from {
-		transform: translateY(-30px);
-	}
-	to {
-		transform: translateY(0);
-	}
-}
-</style>
