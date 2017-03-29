27d71b148d6edb81f8314df72327e7277229a262
Austin <austin@thisisvisceral.com>
2017-03-29 10:54:56 -0700

    Modularizing store.js file

--- src/store/store.js
+++ src/store/store.js
@@ -2,27 +2,22 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import counter from './modules/counter';
 
+// import getters
+import * as getters from './getters';
+// import mutations
+import * as mutations from './mutations';
+// import actions
+import * as actions from './actions';
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
         value: 0
     },
-    getters: {
-        value: state => {
-            return state.value;
-        }
-    },
-    mutations: {
-        updateValue: (state, payload) => {
-            state.value = payload; 
-        }
-    },
-    actions: {
-        updateValue: ({commit}, payload) => {
-            commit('updateValue', payload);
-        }
-    },
+    getters,
+    mutations,
+    actions,
     modules: {
         counter
     }
