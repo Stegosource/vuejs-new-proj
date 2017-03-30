c96a8ea0f062f05386ce927b60873fa333ba3078
Austin <austin@thisisvisceral.com>
2017-03-30 16:13:32 -0700

    Setting up portfolio to show stocks in state.

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
         <h1>The Portfolio/Portfolio Component</h1>
+        <div class="row">
+            <appStock v-for="stock in stocks" :stock="stock"></appStock>
+        </div>
     </div>
 </template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import Stock from './Stock.vue';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+        components: {
+            appStock: Stock
+        }
+    }    
+</script>
