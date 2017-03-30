88fde0e2d04d13e8a3f129c169959530d642a457
Austin <austin@thisisvisceral.com>
2017-03-30 10:57:06 -0700

    Setting up project components structure

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1>The Header Component</h1>
+        <h1>The Home Component</h1>
     </div>
 </template>
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>The Portfolio/Portfolio Component</h1>
+    </div>
+</template>
