b89be9f76e24b493814c89b205dcd3cf9383dc0f
Austin <austin@thisisvisceral.com>
2017-03-31 15:58:55 -0700

    Connecting to firebase and saving data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,8 +23,8 @@
 							aria-expanded="false"
 							@click="isDropdownOpen = !isDropdownOpen">Save & Load <span class="caret"></span></a>
 						<ul class="dropdown-menu">
-							<li><a href="#">Save Data</a></li>
-							<li><a href="#">Load Data</a></li>
+							<li><a href="#" @click="saveData">Save Data</a></li>
+							<li><a href="#" @click="">Load Data</a></li>
 						</ul>
 					</li>
 				</ul>
@@ -53,6 +53,14 @@
 			]),
 			endDay() {
 				this.randomizStocks();
+			},
+			saveData() {
+				const data = {
+					funds: this.$store.getters.funds,
+					stockPortfolio: this.$store.getters.stockPortfolio,
+					stocks: this.$store.getters.stocks
+				}
+				this.$http.put('data.json', data);
 			}
 		}
 	}
